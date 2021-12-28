bb683120f631d50bb39d8d902f150f5b3a9d8f5c
Patrycja <patty.zaw@gmail.com>
2021-12-28 22:05:57 +0100

    zad First Carsapp

---
+++ admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from carsapp.models import Car, NewRate
+
+# Register your models here.
+admin.site.register(Car)
+admin.site.register(NewRate)
---
+++ database_cars.py
@@ -0,0 +1,5 @@
+import requests, json
+# url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json'
+# r = (requests.get(url))
+# print(r.text)
+
